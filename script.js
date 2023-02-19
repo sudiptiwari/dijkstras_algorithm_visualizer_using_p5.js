f23928fd6676ecfa66f22540af2062b5b498aa0b
Sudip Tiwari <sudip10063@gmail.com>
2023-02-19 23:56:52 +0545

    Add, link and style script.js to create bg canvas

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-// alert("Tutor's Section. JavaScript is FUN!");
+function setup() {
+    let cnv = createCanvas(windowWidth / 1.2, windowHeight / 1.2);
+    // cnv.position(100,100);
+    cnv.position(windowWidth / 2 - width / 2, windowHeight / 2 - height / 2); // set the position of the canvas to the center of the window
+    cnv.class('canvas'); // use .canvas { } to style in css
+}
 
+function draw() {
+    background(40,40,40);
+    ellipse(200, 200, 50, 50);
+}
