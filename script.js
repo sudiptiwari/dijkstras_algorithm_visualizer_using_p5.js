e142fed9a9bb8a64b4f721f7dd81cdc74e716032
Sudip Tiwari <sudip10063@gmail.com>
2023-03-02 23:01:14 +0545

    add checkboxes for node,edge,find but still no css class for checkboxes

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 function setup() {
     let cnv = createCanvas(windowWidth / 1.2, windowHeight / 1.2);
-    // cnv.position(100,100);
     cnv.position(windowWidth / 2 - width / 2, windowHeight / 2 - height / 2); // set the position of the canvas to the center of the window
     cnv.class('canvas'); // use .canvas { } to style in css
+
+    let checkboxes = new CheckBoxes(0, 60); // parameters indicate the position of checkboxes
+    checkboxes.display();
+    // checkboxes.addClass('checkbox'); // use .checkboxes { } to style in CSS
 }
 
 function draw() {
-    background(40,40,40);
-    ellipse(200, 200, 50, 50);
+    background(40, 40, 40);
+}
+
+
+class CheckBoxes {
+    constructor(x, y) {
+        this.nodeCheckbox = createCheckbox(' Draw Node', false);
+        this.edgeCheckbox = createCheckbox(' Draw Edge', false);
+        this.shortestPathCheckbox = createCheckbox(' Find Path', false);
+        this.x = x;
+        this.y = y;
+    }
+
+    display() {
+        this.nodeCheckbox.position(this.x, this.y);
+        this.edgeCheckbox.position(this.x, this.y + 30);
+        this.shortestPathCheckbox.position(this.x, this.y + 60);
+    }
 }
