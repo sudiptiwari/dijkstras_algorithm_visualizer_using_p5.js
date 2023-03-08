9a316804a2d638d527339c72fa4ebf457c6a929f
Sudip Tiwari <sudip10063@gmail.com>
2023-03-08 16:20:37 +0545

    code upto starting node selection

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let cnv;
 let checkboxes;
 let circles = []; // Array to store the circles
+let edges = []; // Array to store the edges
+let startingCircle;
+let endCircle;
+let edge; // edge variable to hold details inside drawing edges
 function setup() {
     cnv = createCanvas(windowWidth / 1.2, windowHeight / 1.2);
     cnv.position(windowWidth / 2 - cnv.width / 2, windowHeight / 2 - cnv.height / 2); // set the position of the canvas to the center of the window
@@ -14,42 +18,46 @@ function setup() {
 
 function draw() {
     background(40, 40, 40);
-    // mousePressed();
     // Loop through the array and display all the circles
     for (let i = 0; i < circles.length; i++) {
         let circle = circles[i];
         // noFill();
-        fill(40,40,40);
+        if (circle == startingCircle) {
+            fill(255, 0, 0); // Set fill color to red if selected
+        }
+        else if (circle == endCircle) {
+            fill(0, 255, 0);
+        }
+        else { fill(40, 40, 40); }
         stroke(255);
         strokeWeight(2);
         ellipse(circle.x, circle.y, 50, 50);
+
 
         textAlign(CENTER, CENTER);
         textSize(32);
         fill(255);
         noStroke();
-        text(i+1, circle.x, circle.y);
+        text(i + 1, circle.x, circle.y);
+
     }
 }
 
-function isMouseClickedInsideCanvas()
-{
-    if( mouseIsPressed &&
-    mouseX >= (0) &&
-    mouseX <= (cnv.width) &&
-    mouseY >= (0) &&
-    mouseY <= (cnv.height) )
-    {
+function isMouseClickedInsideCanvas() {
+    if (/*mouseIsPressed &&*/
+        mouseX >= (0) &&
+        mouseX <= (cnv.width) &&
+        mouseY >= (0) &&
+        mouseY <= (cnv.height)) {
         return true;
     }
-    else
-    {
+    else {
         return false;
     }
 }
 
 function mousePressed() {
-    console.log(isMouseClickedInsideCanvas());
+    // creating nodes
     if (isMouseClickedInsideCanvas() && checkboxes.nodeCheckbox.checked() && !checkboxes.edgeCheckbox.checked() && !checkboxes.shortestPathCheckbox.checked() && mouseIsPressed) {
         let circle = {
             x: mouseX,
@@ -59,8 +67,39 @@ function mousePressed() {
         circles.push(circle);
     }
 
-    if (isMouseClickedInsideCanvas() && !checkboxes.nodeCheckbox.checked() && checkboxes.edgeCheckbox.checked() && !checkboxes.shortestPathCheckbox.checked() && mouseIsPressed) {
-        
+    // drawing edges
+    if (isMouseClickedInsideCanvas() && !checkboxes.nodeCheckbox.checked() && checkboxes.edgeCheckbox.checked() && !checkboxes.shortestPathCheckbox.checked()) {
+        if (mousePressed) {
+            for (let i = 0; i < circles.length; i++) {
+                let circle = circles[i];
+                let d = dist(mouseX, mouseY, circle.x, circle.y);
+                if (d < 25) {
+                    // If the mouse is inside the circle, select it
+                    startingCircle = circle;
+                    // startingCircle.fillColor(255,0,0);
+                    edge = new Edge(startingCircle.x, startingCircle.y);
+                    break;
+                }
+            }
+            if(mouseReleased)
+            {
+                for (let i = 0; i < circles.length; i++) {
+                    let circle = circles[i];
+                    let d = dist(mouseX, mouseY, circle.x, circle.y);
+                    if (d < 25) {
+                        // If the mouse is inside the circle, select it
+                        endCircle = circle;
+                        // endCircle.fillColor(0,255,0);
+                        console.log(`End circle detected`);
+                        edge.endX = endCircle.x;
+                        edge.endY = endCircle.y;
+                        break;
+                    }
+                }
+            }
+        }
+
+        // let edge = new Edge()
     }
 }
 
@@ -79,8 +118,18 @@ class CheckBoxes {
         this.edgeCheckbox.position(this.x, this.y + 30);
         this.shortestPathCheckbox.position(this.x, this.y + 60);
     }
-    
-    isNodeChecked() {
-        if (this.nodeCheckbox.checked()) { return true; }
+
+    // isNodeChecked() {
+    //     if (this.nodeCheckbox.checked()) { return true; }
+    // }
+}
+
+class Edge {
+    constructor(startX, startY, endX, endY, weight) {
+        this.startX = startX;
+        this.startY = startY;
+        this.endX = endX;
+        this.endY = endY;
+        this.weight = weight;
     }
 }
